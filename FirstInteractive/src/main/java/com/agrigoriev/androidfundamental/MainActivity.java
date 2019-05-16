ed94a0f3d911ae3abd6565348dcf8baca0c16b54
Alexander Grigoriev <work.off.on@gmail.com>
2019-05-16 21:59:17 +0300

    01.2 Part A. Coding Challenge : add Landscape mode

--- FirstInteractive/src/main/java/com/agrigoriev/androidfundamental/MainActivity.java
+++ FirstInteractive/src/main/java/com/agrigoriev/androidfundamental/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
         mCountTextView = findViewById(R.id.tvCount);
 
         counter = Integer.valueOf(mCountTextView.getText().toString());
