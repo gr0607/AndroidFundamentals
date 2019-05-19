a28a141939ed39ca47a6ff4b06fa02c26629074e
Alexander Grigoriev <work.off.on@gmail.com>
2019-05-19 07:59:56 +0300

    01.3 Text and Scrolling views

---
+++ 01_3_text_and_scrolling/src/main/java/com/agrigoriev/a01_3_text_and_scrolling/MainActivity.java
@@ -0,0 +1,13 @@
+package com.agrigoriev.a01_3_text_and_scrolling;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
