99cab9e7d281806e90f643e140e4ba35d997cf20
Alexander Grigoriev <work.off.on@gmail.com>
2019-06-09 16:56:55 +0300

    safe_home_app (add User Input elements for the second screen)

--- safe_home/src/main/java/com/agrigoriev/safe_home/OrderActivity.java
+++ safe_home/src/main/java/com/agrigoriev/safe_home/OrderActivity.java
@@ -3,7 +3,10 @@ package com.agrigoriev.safe_home;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -17,5 +20,34 @@ public class OrderActivity extends AppCompatActivity {
                 intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = findViewById(R.id.order_text_view);
         textView.setText(message);
+    }
+
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+
+        switch (view.getId()) {
+            case R.id.rb_cash:
+                if (checked) {
+                    displayToast(getString(R.string.cash));
+                }
+                break;
+            case R.id.rb_credit_card:
+                if (checked) {
+                    displayToast(getString(R.string.credit_card));
+                }
+                break;
+            case  R.id.rb_bank_account:
+                if (checked) {
+                    displayToast(getString(R.string.bank_account));
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message,
+                Toast.LENGTH_SHORT).show();
     }
 }
