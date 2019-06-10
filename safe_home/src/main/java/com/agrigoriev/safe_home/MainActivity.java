2f0ef950172b80d096dbbb7f4662b77a58cdbe96
Alexander Grigoriev <work.off.on@gmail.com>
2019-06-10 21:48:36 +0300

    safe_home_app (add menu)

--- safe_home/src/main/java/com/agrigoriev/safe_home/MainActivity.java
+++ safe_home/src/main/java/com/agrigoriev/safe_home/MainActivity.java
@@ -3,7 +3,6 @@ package com.agrigoriev.safe_home;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -28,9 +27,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, OrderActivity.class);
-                intent.putExtra(EXTRA_MESSAGE, mOrderMessage);
-                startActivity(intent);
+              startOrderActivity();
             }
         });
     }
@@ -47,12 +44,22 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+       switch (item.getItemId()) {
+           case R.id.action_contact :
+               displayToast(getString(R.string.action_contact_message));
+               break;
+           case R.id.action_favorites :
+               displayToast(getString(R.string.action_favorites_message));
+               break;
+           case R.id.action_status :
+               displayToast(getString(R.string.action_status_message));
+               break;
+           case R.id.action_order :
+               startOrderActivity();
+               break;
+           default:
+               break;
+       }
 
         return super.onOptionsItemSelected(item);
     }
@@ -74,5 +81,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void displayToast(String message) {
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    public void startOrderActivity() {
+        Intent intent = new Intent(MainActivity.this, OrderActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, mOrderMessage);
+        startActivity(intent);
     }
 }
